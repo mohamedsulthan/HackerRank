896ffee5a1f3fa3e60a7d826b735e44500fd12a9
Mohmaed Sulthan <64527520+mohamedsulthan@users.noreply.github.com>
2022-10-03 11:40:42 +0530

    Create Solution.java

---
+++ Java Int to String./Solution.java
@@ -0,0 +1,23 @@
+/*You are given an integer , you have to convert it into a string.
+
+Please complete the partially completed code in the editor. If your code successfully converts  into a string  the code will print "Good job". Otherwise it will print "Wrong answer".
+
+ can range between  to  inclusive.*/
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        String s=Integer.toString(n);
+        if(s==s){
+            System.out.println("Good job");
+        }
+        else{
+            System.out.println("Wrong answer");
+        }
+    }
+}
